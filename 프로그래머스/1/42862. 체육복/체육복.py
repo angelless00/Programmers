662206d38b826c0dcd3c474ae5ad290d08a377c7
KIM A RAN <angelless00@naver.com>
2025-02-05 17:08:41 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -0,0 +1,30 @@
+def solution(n, lost, reserve):
+    answer1,answer2=n,n
+    yes=[]
+    for lost_num in lost:
+        if lost_num in reserve:
+            yes.append(lost_num)
+    for d in yes:
+        reserve.remove(d)
+        lost.remove(d)
+            
+    reserve1=sorted(reserve).copy()
+    reserve2=sorted(reserve).copy()
+    lost=sorted(lost).copy()       
+
+    for lost_num in lost:
+        if lost_num-1 in reserve1:
+            reserve1.remove(lost_num-1)
+        elif lost_num+1 in reserve1:
+            reserve1.remove(lost_num+1)
+        else:
+            answer1-=1
+            
+        if lost_num+1 in reserve2:
+            reserve2.remove(lost_num+1)
+        elif lost_num-1 in reserve2:
+            reserve2.remove(lost_num-1)
+        else:
+            answer2-=1
+        
+    return max(answer1,answer2)
